9cdcf602092a4a65d43757042d81f47eb9348add
LanaKolisnyk <svkolesnik@gmail.com>
2022-10-31 21:23:01 +0200

    moved homework from Codesandbox to VS Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <div className="container weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control search-input"
+              id="search-input"
+            />
+          </div>
+          <div className="col-3 p-0">
+            <input
+              type="submit"
+              className="btn btn-primary w-100"
+              id="searchbutton"
+              value="Search"
+            />
+          </div>
+        </div>
+      </form>
+
+      <div className="row">
+        <div className="col-sm-6  WeatherInfo">
+          <h1 id="location">
+            Kyiv{" "}
+            <img
+              id="icon"
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Cloudy"
+              className="temperature-container float-left"
+            ></img>
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          {" "}
+          <span className="temperature unit" id="tempnow">
+            15
+          </span>
+          <span className="unit"> °C</span>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <ul>
+            <li>
+              Last updated:
+              <strong>
+                <div id="today">07.10.2022, 14:09</div>
+              </strong>
+              <div id="wea-dis"></div>
+            </li>
+            <li>
+              <div>
+                Humidity:
+                <strong>
+                  <span id="humidity">10</span>%
+                </strong>
+                , Wind:
+                <strong>
+                  <span id="wind">5</span> m/s
+                </strong>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <footer>
+        This project was coded by Lana Kolisnyk and is         <a
+          href="https://github.com/LanaKolisnyk/Vanilla-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on GitHub
+        </a> 
+        and 
+        <a
+          href="https://glowing-salmiakki-cd3414.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         hosted on Netlify
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
